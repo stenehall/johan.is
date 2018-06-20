44f9ba0d79cbb7845618d052a12b0c9b092a3524
Johan Stenehall <stenehall@gmail.com>
2018-06-20 08:29:57 +0200

    Proper fonts

--- src/index.jsx
+++ src/index.jsx
@@ -67,28 +67,18 @@ export class Index extends React.Component {
               __html: `
               WebFontConfig = {
                 google: {
-                  //Fanwood+Text:400,400i|Quattrocento:400,700
                   families: ['Quattrocento:400', 'Fanwood Text:400']
                 }
-              };
-          
+              }
               (function(d) {
-                  var wf = d.createElement('script'), s = d.scripts[0];
-                  wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
-                  wf.async = true;
-                  s.parentNode.insertBefore(wf, s);
+                  var wf = d.createElement('script'), s = d.scripts[0]
+                  wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js'
+                  wf.async = true
+                  s.parentNode.insertBefore(wf, s)
               })(document)
 
       if ('serviceWorker' in navigator) {
-        window.addEventListener('load', function() {
-          navigator.serviceWorker.register('/sw.js', {scope: './'}).then(function(registration) {
-            // Registration was successful
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
-          }, function(err) {
-            // registration failed :(
-            console.log('ServiceWorker registration failed: ', err);
-          });
-        });
+        window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', {scope: './'}).then(registration => {}, err => {})})
       }
       `
             }}
