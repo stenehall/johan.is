ddf0d89389206dfeb8eda758ee39ce7514e39041
Johan Stenehall <stenehall@gmail.com>
2018-06-23 23:23:41 +0200

    Improve inline list

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -28,29 +28,11 @@ export default ({ children }) => (
         sizes="32x32"
         href="/static/favicon-32x32.png"
       />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/static/favicon-16x16.png"
-      />
       <link rel="manifest" href="/static/site.webmanifest" />
-      <link
-        rel="mask-icon"
-        href="/static/safari-pinned-tab.svg"
-        color="#000000"
-      />
-      <meta name="msapplication-TileColor" content="#ffc40d" />
       <meta name="theme-color" content="#ffffff" />
 
       <meta property="og:image:width" content="1648" />
       <meta property="og:image:height" content="863" />
-      <meta
-        property="og:description"
-        content="A developer building things, currently for Volvo creating new things."
-      />
-      <meta property="og:title" content="Johan Stenehall" />
-      <meta property="og:url" content="https://johan.is" />
       <meta
         property="og:image"
         content="https://johan.is/static/og-image.jpg"
